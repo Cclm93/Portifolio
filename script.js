1f71777d6828618e74dae639d5a5c12ea4c5dd8b
CaioM <ccldm200@gmail.com>
2023-07-18 20:37:35 -0300

    Jquery
    Foi adicionada as primeiras versões do Jquery no site

--- script.js
+++ script.js
@@ -1,21 +1,73 @@
-document.querySelectorAll('a').forEach(link =>{ 
-    const conteudo = document.getElementById('conteudo')   
-    
-    link.onclick = function(e) {
-
-        e.preventDefault()  
-         
-            fetch(link.href)
-                .then(resp => resp.text())
-                .then (html => conteudo.innerHTML = html)
-    }}
-)
-
-i.style.display = 'block'
-i.style.position.y = 200 
-
 $(document).ready(function(){
-    $("p").click(function(){
+  $("p").click(function(){
       $(this).hide();
+ });
+ });
+
+ $(document).ready(function(){
+
+
+    
+
+    $("#desativar").click(function(){
+           //  $('#hab1').fadeIn();
+           //  $("#div2").fadeIn("slow");
+             $('#hab1').fadeOut();
+             $("#hab2").fadeOut();
+             $("#hab3").fadeOut();
+             $("#hab4").fadeOut();
+             $("#hab5").fadeOut();
+             $("#hab6").fadeOut();
+             $("#hab7").fadeOut();
+             $("#hab8").fadeOut();
+             $("#hab9").fadeOut();
+             $("#hab10").fadeOut();
+             $("#hab11").fadeOut();
+             $("#hab12").fadeOut();
+             $("#hab13").fadeOut();
+             $("#hab14").fadeOut();
+             $("#hab15").fadeOut();
+             $("#hab16").fadeOut();
+             $("#hab17").fadeOut();
+             $("#hab18").fadeOut();
+             $("#hab19").fadeOut();
+             $("#hab20").fadeOut();
+             $("#hab21").fadeOut();
+             $("#hab22").fadeOut();
+             $("#hab23").fadeOut();
+             $("#hab24").fadeOut();
+            }),
+
+
+         $("#ativar").click(function(){
+           
+             $('#hab1').fadeIn(1000);
+             $("#hab2").fadeIn(1000);
+             $("#hab3").fadeIn(1000);
+             $("#hab4").fadeIn(1000);
+             $("#hab5").fadeIn(1000);
+             $("#hab6").fadeIn(1000);
+             $("#hab7").fadeIn(1000);
+             
+             $("#hab8").fadeIn(2000);
+             $("#hab9").fadeIn(2000);
+             $("#hab10").fadeIn(2000);
+            
+             $("#hab11").fadeIn(3000);
+             $("#hab12").fadeIn(3000);
+             $("#hab13").fadeIn(3000);
+             $("#hab14").fadeIn(3000);
+             $("#hab15").fadeIn(3000);
+             $("#hab16").fadeIn(3000);
+             $("#hab17").fadeIn(3000);
+            
+             $("#hab18").fadeIn(4000);
+             $("#hab19").fadeIn(4000);
+             $("#hab20").fadeIn(4000);
+             $("#hab21").fadeIn(4000);
+             $("#hab22").fadeIn(4000);
+             $("#hab23").fadeIn(4000);
+             $("#hab24").fadeIn(4000);
+          
     });
   });
